2731cfab38f3d40e13175d779187786619e9f6d0
Daryu <dev_daryu@kakao.com>
2023-03-28 11:08:17 +0900

    2023-03-28 11:08 Added Login.tsx and Login.module.scss

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,11 @@
+import './Login.module.scss';
+
+function Login() {
+    return (
+        <div className="wrapper">
+            <p className={styles[fs-24]}>CodeUnity</p>
+        </div>
+    );
+}
+
+export default Login;
