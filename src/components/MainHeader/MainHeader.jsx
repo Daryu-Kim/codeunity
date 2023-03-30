4bd9884bbbbfce757b7a2a6aa5afb195d003ac02
baekseungho <vudrkd1580@naver.com>
2023-03-31 03:23:12 +0900

    navbar styling

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -12,20 +12,22 @@ import {
 const MainHeader = () => {
   return (
     <div className={styles.wrapper}>
-      <img src={logo} className={styles.logo} />
+      <div className={styles.logoBox}>
+        <img src={logo} className={styles.logo} />
+      </div>
       <div className={styles.leftMenu}>
         <div className={styles.tabMenu}>
           <FontAwesomeIcon icon={faCircleQuestion} />
           개발자 QnA
         </div>
-        <div>
+        <div className={styles.tabMenu}>
           <FontAwesomeIcon icon={faEnvelope} />
           문의하기
         </div>
       </div>
       <div className={styles.rightMenu}>
         <FontAwesomeIcon icon={faComments} className={styles.navIcon} />
-        <div className={styles.myProfile}>MY Profile</div>
+        <div className={styles.myProfile}></div>
       </div>
     </div>
   );
