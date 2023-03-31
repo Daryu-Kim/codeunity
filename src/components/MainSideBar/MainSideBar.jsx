da20f81e5b8cfac23287cb38e1d48289263489b3
baekseungho <vudrkd1580@naver.com>
2023-03-31 21:19:52 +0900

    sidebar

--- src/components/MainSideBar/MainSideBar.jsx
+++ src/components/MainSideBar/MainSideBar.jsx
@@ -3,19 +3,44 @@ import styles from "./MainSideBar.module.scss";
 import font from "../../styles/Font.module.scss";
 import { faMoon } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGear } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCodeBranch,
+  faGear,
+  faMagnifyingGlass,
+  faUserGroup,
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const MainSideBar = () => {
   return (
     <div className={styles.mainSideBar}>
-      <FontAwesomeIcon
-        icon={faMoon}
-        className={`${styles.sidebarIcon} ${font.fs_28} ${font.fc_accent}`}
-      />
-      <FontAwesomeIcon
-        icon={faGear}
-        className={`${styles.sidebarIcon} ${font.fs_28} ${font.fc_accent}`}
-      />
+      <div className={styles.iconBox}>
+        <div className={styles.onIconBox}>
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className={`${styles.sidebarIcon} ${font.fs_24} ${font.bg}`}
+          />
+          <FontAwesomeIcon
+            icon={faUserGroup}
+            className={`${styles.sidebarIcon} ${font.fs_24} ${font.bg}`}
+          />
+          <FontAwesomeIcon
+            icon={faCodeBranch}
+            className={`${styles.sidebarIcon} ${font.fs_24} ${font.bg}`}
+          />
+          <FontAwesomeIcon
+            icon={faGithub}
+            className={`${styles.sidebarIcon} ${font.fs_24} ${font.bg}`}
+          />
+        </div>
+        <div className={styles.underIconBox}>
+          <FontAwesomeIcon
+            icon={faGear}
+            className={`${styles.sidebarIcon} ${font.fs_28} ${font.bg}`}
+          />
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 };
