ec229a3838ec4eb4a6c0ba3b591bc48bfe7cf2a1
baekseungho <vudrkd1580@naver.com>
2023-03-31 18:58:20 +0900

    sidebar

--- src/components/MainSideBar/MainSideBar.jsx
+++ src/components/MainSideBar/MainSideBar.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "./MainSideBar.module.scss";
 import font from "../../styles/Font.module.scss";
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
 
 const MainSideBar = () => {
   return (
     <div className={styles.mainSideBar}>
-      asdf
+      <FontAwesomeIcon
+        icon={faMoon}
+        className={`${styles.sidebarIcon} ${font.fs_28} ${font.fc_accent}`}
+      />
+      <FontAwesomeIcon
+        icon={faGear}
+        className={`${styles.sidebarIcon} ${font.fs_28} ${font.fc_accent}`}
+      />
     </div>
   );
 };
