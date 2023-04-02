98995572b240f1fb02f3b9ce6d3378c726e0784c
Daryu <dev_daryu@kakao.com>
2023-04-03 00:39:51 +0900

    2023-04-03 12:39 Update

--- src/components/MainProfile/MainProfile.jsx
+++ src/components/MainProfile/MainProfile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MainProfile.module.scss";
 import font from "../../styles/Font.module.scss";
 import { useLocation } from "react-router-dom";
@@ -13,6 +13,19 @@ const MainProfile = () => {
     doc(firestore, "Users", state)
   );
   const uid = localStorage.getItem("uid");
+  const [menuTab, setMenuTab] = useState(true);
+
+  const changePostTab = (e) => {
+    if (e.target.checked) {
+      setMenuTab(true);
+    }
+  };
+
+  const changeQnaTab = (e) => {
+    if (e.target.checked) {
+      setMenuTab(false);
+    }
+  };
 
   if (document) {
     return (
@@ -43,21 +56,46 @@ const MainProfile = () => {
             </div>
           </div>
           <div className={styles.nameBox}>
-            <p className={`${font.fs_24} ${font.fw_7}`}>
-              {document.userName}
-            </p>
+            <p className={`${font.fs_24} ${font.fw_7}`}>{document.userName}</p>
             <p className={`${font.fs_16} ${font.fc_sub}`}>
-              {
-                document.userDesc
-                  ? document.userDesc
-                  : "자기소개가 없습니다!"
-              }
+              {document.userDesc ? document.userDesc : "자기소개가 없습니다!"}
             </p>
           </div>
           <div className={styles.followBox}>
-            <div className={styles.followerBox}></div>
-            <div className={styles.followingBox}></div>
+            <div className={styles.followerBox}>
+              <p className={`${styles.followTitle} ${font.fs_14} ${font.fw_7}`}>
+                팔로우
+              </p>
+              <p
+                className={`${styles.followTitle} ${font.fs_14} ${font.fw_7} ${font.fc_accent}`}
+              >
+                {document.followerCount}
+              </p>
+            </div>
+            <div className={styles.followingBox}>
+              <p className={`${styles.followTitle} ${font.fs_14} ${font.fw_7}`}>
+                팔로잉
+              </p>
+              <p
+                className={`${styles.followTitle} ${font.fs_14} ${font.fw_7} ${font.fc_accent}`}
+              >
+                {document.followingCount}
+              </p>
+            </div>
           </div>
+        </div>
+        <div className={styles.contBox}>
+          <div className={styles.menuTab}>
+            <input type="radio" name="tab" checked={menuTab == true} id="post" onChange={(e) => changePostTab(e)} className={styles.none} />
+            <label htmlFor="post" className={`${font.fs_16} ${font.fw_7} ${styles.menuTabPost}`}>게시물</label>
+            <input type="radio" name="tab" checked={menuTab == false} id="qna" onChange={(e) => changeQnaTab(e)} className={styles.none} />
+            <label htmlFor="qna" className={`${font.fs_16} ${font.fw_7} ${styles.menuTabQnA}`}>QnA</label>
+          </div>
+          {menuTab ? (
+            <div className={styles.postBox}>asdf</div>
+          ) : (
+            <div className={styles.postBox}>1234</div>
+          )}
         </div>
       </div>
     );
