3179251fc4970a32899acbb0cd512768ca4a41ec
Won Jae Kim <86081049+Daryu-Kim@users.noreply.github.com>
2023-04-03 14:02:53 +0900

    2023-04-03 14:02 Update

--- src/components/MainHome/MainHome.jsx
+++ src/components/MainHome/MainHome.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import MainSideBar from "../../components/MainSideBar/MainSideBar";
 import styles from "./MainHome.module.scss";
 import font from "../../styles/Font.module.scss";
 import baseImg from "../../assets/svgs/352174_user_icon.svg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCode, faFont, faImage, faLink, faVideo, IconDefinition } from '@fortawesome/free-solid-svg-icons';
-import { getAuth, signOut } from '@firebase/auth';
-import { useCollection, useCollectionData, useDocument, useDocumentData } from "react-firebase-hooks/firestore"
+import { getAuth } from '@firebase/auth';
+import { useCollectionData, useDocumentData } from "react-firebase-hooks/firestore"
 import { collection, doc, getFirestore, limit, orderBy, query, where } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
 import { FreeMode } from "swiper";
@@ -35,6 +34,7 @@ const MainHome = () => {
   const [htmlWidth, setHtmlWidth] = useState(0);
   const [postData, setPostData] = useState(null);
   const [popularData, setPopularData] = useState(null);
+  const [modalState, setModalState] = useState(false);
   const navigate = useNavigate();
   useEffect(() => {
     if (document) {
@@ -159,54 +159,16 @@ const MainHome = () => {
     }
   }, [allUID]);
 
-  // const [
-  //   post,
-  //   postLoad,
-  //   postError,
-  //   postSnapshot
-  // ] = useCollectionData(
-  //   query(
-  //     collection(firestore, `/Users/${allUID}`)
-  //   )
-  // );
-  
-  // const [value, load, err] = useDocument(
-  //   doc(firestore, "Users", uid)
-  // );
-  // const [userName, setUserName] = useState("");
-  
-  // const [myDoc, myDocLoad, myDocError] = useDocument(
-  //   doc(firestore, "Users", user.uid)
-  // )
-  // const [userName, setUserName] = useState("");
-  // // const myID = getMyUID();
-  // // const myDoc = getUserData(myID);
-  // // myDoc.then((result) => {
-  // //   myData = result;
-  // //   console.log(myData)
-  // //   setUserName(myData.userName);
-  // // })
-
-  // if (allUID) {
-  //   const allUIDArr = [];
-  //   allUID.forEach((ele) => {
-  //     allUIDArr.push(ele.data());
-  //   })
-  //   console.log(allUIDArr)
-  // }
-  
-
-  // const myData = getMyData();
-  // myData.then((result) => {
-  //   console.log(result);
-  // });
-
   const profileClick = (userID) => {
     navigate("/profile", {
       state: userID,
       replace: true,
     });
   }
+
+  const showModal = () => {
+    setModalState(true);
+  };
 
   const renderBlockData = () => {
     const mapBlockData = blockBoxData.map(
@@ -231,56 +193,69 @@ const MainHome = () => {
     
   }
 
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.box}>
-        <div className={`${styles.writePostBtn}`}>
-          <div className={styles.writePostTopBox}>
-            <div
-              className={styles.writePostTopImg}
-              onClick={() => profileClick(uid)}
-            ></div>
-            <div className={styles.writePostTopInputBox}>
-              <p className={`${font.fs_14} ${font.fc_sub_light} ${styles.writePostTopName}`}>
-                {htmlWidth > 767 ?
-                `${userName}님, 무슨 생각중인가요?` :
-                `글쓰기..`}
-              </p>
+  if (document) {
+    return (
+      <div className={styles.wrapper}>
+        {/* {
+          modalState &&
+
+        } */}
+        <div className={styles.box}>
+          <div className={`${styles.writePostBtn}`}>
+            <div className={styles.writePostTopBox}>
+              <div
+                className={styles.writePostTopImg}
+                onClick={() => profileClick(uid)}
+                style={
+                  document.userImg ?
+                  {backgroundImage: `url(${document.userImg})`} :
+                  {backgroundImage: `url(${baseImg})`}
+                }
+              ></div>
+              <div className={styles.writePostTopInputBox} onClick={showModal}>
+                <p className={`${font.fs_14} ${font.fc_sub_light} ${styles.writePostTopName}`}>
+                  {htmlWidth > 767 ?
+                  `${userName}님, 무슨 생각중인가요?` :
+                  `글쓰기..`}
+                </p>
+              </div>
+            </div>
+            <hr className={styles.hr} />
+            <div className={styles.writePostBlockBox}>
+              {renderBlockData()}
             </div>
           </div>
-          <hr className={styles.hr} />
-          <div className={styles.writePostBlockBox}>
-            {renderBlockData()}
+
+          <div className={styles.popularBox}>
+            <p
+              className=
+              {`
+                ${font.fs_18}
+                ${font.fw_7}
+              `}
+            >
+              인기 프로필을 팔로우해보세요!
+            </p>
+            <Swiper
+              slidesPerView={'auto'}
+              spaceBetween={8}
+              freeMode={true}
+              modules={[FreeMode]}
+              id={styles.popularSwiper}
+            >
+              {popularData}
+            </Swiper>
+          </div>
+          
+          <div className={styles.postBox}>
+            {postData}
           </div>
         </div>
-
-        <div className={styles.popularBox}>
-          <p
-            className=
-            {`
-              ${font.fs_18}
-              ${font.fw_7}
-            `}
-          >
-            인기 프로필을 팔로우해보세요!
-          </p>
-          <Swiper
-            slidesPerView={'auto'}
-            spaceBetween={8}
-            freeMode={true}
-            modules={[FreeMode]}
-            id={styles.popularSwiper}
-          >
-            {popularData}
-          </Swiper>
-        </div>
-        
-        <div className={styles.postBox}>
-          {postData}
-        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  
 }
 
 export default MainHome;
