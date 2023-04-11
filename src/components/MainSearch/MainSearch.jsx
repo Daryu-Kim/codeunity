656a26c5c2cb9c70b20645d262d0f3f915947743
baekseungho <vudrkd1580@naver.com>
2023-04-11 23:01:25 +0900

    search

--- src/components/MainSearch/MainSearch.jsx
+++ src/components/MainSearch/MainSearch.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { useState } from "react";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 
 const MainSearch = () => {
-  return <div>MainSearch</div>;
-};
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+
+    const db = getFirestore();
+    const q = query(collection(db, "Users"), where("name", "==", query));
+    const querySnapshot = await getDocs(q);
+    const results = querySnapshot.docs.map((doc) => doc.data());
+    setSearchResults(results);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSearch}>
+        <input type="text" value={query} onChange={handleInputChange} />
+        <button type="submit">Search</button>
+      </form>
+      {searchResults.length > 0 ? (
+        <ul>
+          {searchResults.map((user) => (
+            <li key={user.uid}>{user.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No results found</p>
+      )}
+    </div>
+  );
+};
 export default MainSearch;
